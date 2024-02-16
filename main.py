4754c7d13955033c0cefb6ac4570de781bdac45d
Evil Developer <adrulub@yandex.ru>
2024-02-16 18:18:21 +0300

    solved_problem

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, url_for, request, render_template
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/<title>')
-@app.route('/index/<title>')
-def main_page(title):
-    return render_template('base.html', title=title)
+@app.route('/list_prof/<list>')
+def main_page(list):
+    with open("static/profs.json", "rt", encoding="utf8") as f:
+        prof_list = json.loads(f.read())
+    return render_template('professions_pattern.html', list=list, prof_list=prof_list)
 
 
 if __name__ == '__main__':
