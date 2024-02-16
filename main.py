a61bfd66d4c566cb2232ee1385742feff772a07c
Evil Developer <adrulub@yandex.ru>
2024-02-16 19:12:48 +0300

    solved_problem

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask, url_for, request, render_template
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/<title>')
-@app.route('/index/<title>')
-def main_page(title):
-    return render_template('base.html', title=title)
+@app.route('/answer')
+@app.route('/auto_answer')
+def main_page():
+    with open("static/personal_data.json", "rt", encoding="utf8") as f:
+        data_list = json.loads(f.read())
+    return render_template('data_pattern.html', title='Анкета', list=data_list)
 
 
 if __name__ == '__main__':
