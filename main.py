bca0983b355b678340d58a63a35590045d429d0b
Evil Developer <adrulub@yandex.ru>
2024-02-17 14:05:35 +0300

    solved_problem

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask, url_for, request, render_template
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/<title>')
-@app.route('/index/<title>')
-def main_page(title):
-    return render_template('base.html', title=title)
+@app.route('/distribution')
+def distribution():
+    crew_list = ['Ридли Скотт', 'Энди Уир', 'Марк Уотни', 'Венката Капур', 'Тедди Сандерс', 'Шон Бин']
+    return render_template('distribution_pattern.html', crew_list=crew_list)
 
 
 if __name__ == '__main__':
